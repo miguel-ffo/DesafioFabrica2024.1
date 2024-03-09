113e897f5dec6ada5481820eb741ef0f0c3252bc
Miguel Figueiredo <miguelgamesjua@gmail.com>
2024-03-08 22:09:55 -0300

    2
    base 2

--- filme/models.py
+++ filme/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Filme(models.Model):
+    titulo = models.CharField(max_length=30)
+    sinopse = models.CharField(max_length=1000)
+    genero = models.ForeignKey(max_length=30)
+    ano =  models.IntegerField(max_length=4)
+    diretor = models.CharField(max_length=150)
+
+
+    def __str__(self):
+        return self.titulo
+
+asd
